d6a4e0dae9abe21dce0b674ba46de45c16fcee61
odiwuorlameck2019gmail <odiwuorlameck2019@gmail.com>
2021-07-26 08:23:02 -0700

    HandDetection

---
+++ hand detection/hand detection.py
@@ -0,0 +1,22 @@
+import cv2 
+import mediapipe as mp 
+import time 
+cap=cv2.VideoCapture(0)
+mpHands=mp.solutions.hands
+hands=mpHands.Hands()
+mpDraw=mp.solutions.drawing_utils
+pTime=0
+cTime=0
+
+while True:
+    success,img=cap.read()
+    imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results=hands.process(imgRGB)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
+    cTime=time.time()
+    fps=1/(cTime-pTime)
+    pTime=cTime
+    cv2.imshow("image ",img)
+    cv2.waitKey(1)
